537aed3f015a9e7de892a7d21fe310ef295c6557
Everezze <83906211+Everezze@users.noreply.github.com>
2024-05-10 00:42:16 +0200

    adding new tasks and their removal feature added

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const activeFilter = document.querySelector("main>aside div span:nth-child(2)");
 const completedFilter = document.querySelector("main>aside div span:nth-child(3)");
 const allFilter = document.querySelector("main>aside div span:nth-child(1)");
 const tasksSection = document.querySelector("section");
+const articles = Array.from(document.querySelectorAll("section article"));
+const input = document.querySelector("input");
 
 console.log(checkMarks);
 console.log(tasks);
@@ -14,39 +16,71 @@ console.log(crossIcons);
 console.log(taskCompletedCleaner);
 console.log(marksAndTasks);
 
-marksAndTasks.forEach(function(element){
+function addActive(element){
 	element.addEventListener("click",function(){
 		element.parentElement.classList.toggle("active");
 	});
-});
+};
+
+function enableActiveFilter(){
+	tasksSection.classList.toggle("active-filter");
+	tasksSection.classList.remove("completed-filter");
+};
+
+function enableCompletedFilter(){
+	tasksSection.classList.toggle("completed-filter");
+	tasksSection.classList.remove("active-filter");
+};
+
+function enableallFilter(){
+	tasksSection.classList.remove("completed-filter");
+	tasksSection.classList.remove("active-filter");
+};
+
+function enableRemoveArticle(element){
+	element.addEventListener("click",function(){
+		let articleToRemove = this.parentElement;
+		articleToRemove.parentElement.removeChild(articleToRemove);
+	});
+};
+
+marksAndTasks.forEach(addActive);
 
 taskCompletedCleaner.addEventListener("click",function(){
-	for(let i =0;i<checkMarks.length;i++){
-		if(checkMarks[i].classList.contains("active")){
-			let articleToRemove= checkMarks[i].parentElement;
+	for(let i =0;i<articles.length;i++){
+		if(articles[i].classList.contains("active")){
+			let articleToRemove= articles[i];
 			articleToRemove.parentElement.removeChild(articleToRemove);
 		}
 	}
 });
 
-activeFilter.addEventListener("click",function(){
-	tasksSection.classList.toggle("active-filter");
-	tasksSection.classList.remove("completed-filter");
-});
+activeFilter.addEventListener("click", enableActiveFilter);
+completedFilter.addEventListener("click", enableCompletedFilter);
+allFilter.addEventListener("click",enableallFilter);
+crossIcons.forEach(enableRemoveArticle);
 
-completedFilter.addEventListener("click",function(){
-	tasksSection.classList.toggle("completed-filter");
-	tasksSection.classList.remove("active-filter");
-});
+input.addEventListener("keydown",function(event){
+	if(event.key == "Enter" && this.value != ""){
+		let article = document.createElement("article");
+		let circle = document.createElement("div");
+		let img= document.createElement("img");
+		let span = document.createElement("span");
+		let crossIcon = document.createElement("img");
+		crossIcon.setAttribute("src","images/icon-cross.svg");
+		crossIcon.classList.add("cross-icon");
+		enableRemoveArticle(crossIcon);
+		img.setAttribute("src","images/icon-check.svg");
+		span.textContent = this.value;
+		addActive(span);
+		addActive(circle);
+		circle.classList.add("circle");
+		circle.appendChild(img);
+		article.appendChild(circle);
+		article.appendChild(span);
+		article.appendChild(crossIcon);
+		//articles.push(article);
 
-allFilter.addEventListener("click",function(){
-	tasksSection.classList.remove("completed-filter");
-	tasksSection.classList.remove("active-filter");
-});
-
-crossIcons.forEach(function(element){
-	element.addEventListener("click",function(){
-		let articleToRemove = this.parentElement;
-		articleToRemove.parentElement.removeChild(articleToRemove);
-	});
+		tasksSection.insertBefore(article,taskCompletedCleaner.parentElement);
+	}
 });
