34069e9e53817b4f61da7fdc3e4a8a03c943594b
Everezze <83906211+Everezze@users.noreply.github.com>
2024-05-11 02:47:24 +0200

    (re)placing filters elements responsively, adding more styles

--- script.js
+++ script.js
@@ -3,18 +3,15 @@ const tasks = Array.from(document.querySelectorAll("article span"));
 const marksAndTasks =checkMarks.concat(tasks);
 const crossIcons = document.querySelectorAll("section .cross-icon");
 const taskCompletedCleaner = document.querySelector("section aside span:last-child");
+const filterContainer = document.querySelector("main>aside div");
 const activeFilter = document.querySelector("main>aside div span:nth-child(2)");
 const completedFilter = document.querySelector("main>aside div span:nth-child(3)");
 const allFilter = document.querySelector("main>aside div span:nth-child(1)");
 const tasksSection = document.querySelector("section");
 const articles = Array.from(document.querySelectorAll("section article"));
 const input = document.querySelector("input");
-
-console.log(checkMarks);
-console.log(tasks);
-console.log(crossIcons);
-console.log(taskCompletedCleaner);
-console.log(marksAndTasks);
+const main = document.querySelector("main");
+const secondAside = document.querySelector("main > asidel");
 
 function addActive(element){
 	element.addEventListener("click",function(){
@@ -44,8 +41,30 @@ function enableRemoveArticle(element){
 	});
 };
 
-marksAndTasks.forEach(addActive);
+function createArticle(input){
+	let article = document.createElement("article");
+	let circle = document.createElement("div");
+	let img= document.createElement("img");
+	let span = document.createElement("span");
+	let crossIcon = document.createElement("img");
+	crossIcon.setAttribute("src","images/icon-cross.svg");
+	crossIcon.classList.add("cross-icon");
+	enableRemoveArticle(crossIcon);
+	img.setAttribute("src","images/icon-check.svg");
+	span.textContent = input.value;
+	addActive(span);
+	addActive(circle);
+	circle.classList.add("circle");
+	circle.appendChild(img);
+	article.appendChild(circle);
+	article.appendChild(span);
+	article.appendChild(crossIcon);
+	articles.push(article);
+	
+	return article;
+}
 
+marksAndTasks.forEach(addActive);
 taskCompletedCleaner.addEventListener("click",function(){
 	for(let i =0;i<articles.length;i++){
 		if(articles[i].classList.contains("active")){
@@ -54,7 +73,6 @@ taskCompletedCleaner.addEventListener("click",function(){
 		}
 	}
 });
-
 activeFilter.addEventListener("click", enableActiveFilter);
 completedFilter.addEventListener("click", enableCompletedFilter);
 allFilter.addEventListener("click",enableallFilter);
@@ -62,25 +80,22 @@ crossIcons.forEach(enableRemoveArticle);
 
 input.addEventListener("keydown",function(event){
 	if(event.key == "Enter" && this.value != ""){
-		let article = document.createElement("article");
-		let circle = document.createElement("div");
-		let img= document.createElement("img");
-		let span = document.createElement("span");
-		let crossIcon = document.createElement("img");
-		crossIcon.setAttribute("src","images/icon-cross.svg");
-		crossIcon.classList.add("cross-icon");
-		enableRemoveArticle(crossIcon);
-		img.setAttribute("src","images/icon-check.svg");
-		span.textContent = this.value;
-		addActive(span);
-		addActive(circle);
-		circle.classList.add("circle");
-		circle.appendChild(img);
-		article.appendChild(circle);
-		article.appendChild(span);
-		article.appendChild(crossIcon);
-		//articles.push(article);
-
+		let article = createArticle(this);
 		tasksSection.insertBefore(article,taskCompletedCleaner.parentElement);
+		this.value = "";
 	}
 });
+
+const resObserv = new ResizeObserver(entries => {
+	console.log("entered resize event");
+	console.log(tasksSection.nextElementSibling);
+	for(let entry of entries){
+		if(entry.contentRect.width > 600){
+			taskCompletedCleaner.parentElement.insertBefore(filterContainer,taskCompletedCleaner);
+		}
+		else{
+			tasksSection.nextElementSibling.insertBefore(filterContainer,null);
+		};
+	}
+});
+resObserv.observe(main);
